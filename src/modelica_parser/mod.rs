9cc6650565579fa7274d1c91d68af25450140e99
Brennan Harris <brennan@brennanharris.xyz>
2023-11-17 23:29:24 -0700

    cleaned up warnings

--- src/modelica_parser/mod.rs
+++ src/modelica_parser/mod.rs
@@ -1,9 +1,10 @@
-// use antlr_rust::{
-//     char_stream::CharStream, 
-//     common_token_stream::CommonTokenStream,
-//     error_listener::ConsoleErrorListener, 
-//     tree::LeafNode
-// };
+#[allow(unused_imports)]
+use antlr_rust::{
+    char_stream::CharStream, 
+    common_token_stream::CommonTokenStream,
+    error_listener::ConsoleErrorListener, 
+    tree::LeafNode
+};
 
 mod modelicalexer;
 mod modelicaparser;
@@ -14,3 +15,17 @@ mod modelicalistener;
 // pub struct Test {
 //     pub name: String,
 // }
+
+#[allow(dead_code)]
+pub struct ModelicaErrorListener {
+    pub listener: ConsoleErrorListener,
+}
+
+impl ModelicaErrorListener {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        ModelicaErrorListener {
+            listener: ConsoleErrorListener {},
+        }
+    }
+}
